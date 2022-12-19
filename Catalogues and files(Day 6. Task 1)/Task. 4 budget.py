461c02ee976736a0aad0acd1dbc157a93e842027
airidasbudre <107766475+airidasbudre@users.noreply.github.com>
2022-12-19 20:46:35 +0200

    Add files via upload

--- Catalogues and files(Day 6. Task 1)/Task. 4 budget.py
+++ Catalogues and files(Day 6. Task 1)/Task. 4 budget.py
@@ -9,12 +9,10 @@ import pickle
 
 #1 ivesti pajamas arba išlaidas. Input i file
 
-global budget
-budget = []
 
 while True:
     decision = input("""1 - Add income/expenses,\n2 - Print all income and expenses,\n3 - Print balance
-    """)
+    -""")
     if decision == "2":
         try:
             with open("budget.pkl", 'rb') as failas:
@@ -22,24 +20,22 @@ while True:
         except:
             print("Nėra tokio failo")
             with open("budget.pkl", 'wb') as failas:
+                budget = []
                 pickle.dump(budget, failas)
     if decision == "1":
         with open("budget.pkl", 'rb') as failas:
             budget = pickle.load(failas)
-            income_expenses = input(int("Add amount"))
+            income_expenses = int(input("Add amount"))
             with open("budget.pkl", 'wb') as failas:
                 budget.append(income_expenses)
                 pickle.dump(budget, failas)
-    if decision == 3:
+    if decision == "3":
+        with open("budget.pkl", 'rb') as failas:
+            budget = pickle.load(failas)
         print(sum(budget))
 
 
 
 
-#2 duomenys saugomi faile ir isjungus nedingsta
-
-#3 print rezultata
-
-#4 balansas 
 
 
