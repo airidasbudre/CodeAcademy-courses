f1d647f5ebada27a4f5cdd705f343658644dbb05
airidasbudre <budreckis.airidas@gmail.com>
2023-01-18 23:24:30 +0200

    Update
    Add HTML

--- day_30_django/carservice/library/views.py
+++ day_30_django/carservice/library/views.py
@@ -1,4 +1,15 @@
+from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Car_model, Service, Order, Cars, Orderline
+
 
 def index(request):
-    return HttpResponse("Mano autoservisas!")
+    num_car_model = Car_model.objects.all().count()
+    num_service = Service.objects.all().count()
+
+    context = {
+        "num_car_model" : num_car_model,
+        "num_service" : num_service, 
+    }
+
+    return render(request, 'index.html', context=context)
