eb21a89b7500d2d15a8b4e634cd0c943f06240be
airidasbudre <budreckis.airidas@gmail.com>
2023-01-28 14:46:58 +0200

    creating models

--- mysite2/mysite2/project_managements/models.py
+++ mysite2/mysite2/project_managements/models.py
@@ -1,14 +1,37 @@
 from django.db import models
 
 class Project(models.Model):
-    name = models.CharField('Name', max_length=200)
+    p_name = models.CharField('Project Name', max_length=200)
     start_date = models.DateField('Start date', null=True, blank=True)
     end_date = models.DateField('End date', null=True, blank=True)
     client = models.ForeignKey('Client', on_delete=models.SET_NULL, null=True)
     project_manager = models.ForeignKey('Project manager', on_delete=models.SET_NULL, null=True)
     employees = models.ForeignKey('Employees', on_delete=models.SET_NULL, null=True)
-    
-
+    work = models.ForeignKey('Work', on_delete=models.SET_NULL, null=True)
+    invoice = models.ForeignKey('Invoice', on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
-        return self.name
+        return self.p_name
+    
+class Client(models.Model):
+    name = models.CharField('Name', max_length=200)
+    surname = models.CharField('Surname', max_length=200)
+    company = models.CharField('Company', max_length=200)
+    email = models.CharField('Email', max_length=200)
+    phone = models.IntegerField('Phone', max_length=11)
+
+    def __str__(self):
+        return f'{self.name} {self.surname} {self.company}'
+    
+class Employee(models.Model):
+    name = models.CharField('Name', max_length=200)
+    surname = models.CharField('Surname', max_length=200)
+    position = models.CharField('Surname', max_length=200)
+
+class Work(models.Model):
+    title = models.CharField('Title', max_length=200)
+    note = models.CharField('Note', max_length=200)
+
+class Invoice(models.Model):
+    formating_date = models.DateField('Formating Date', null=True, blank=True)
+    amount = models.FloatField('Amount', max_length=11)
