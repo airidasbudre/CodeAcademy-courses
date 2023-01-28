f3ec9068c0f3a4b30585a4e25b8b307d3228d7f8
airidasbudre <107766475+airidasbudre@users.noreply.github.com>
2023-01-28 14:15:27 +0300

    Update models.py

--- mysite2/mysite2/project_managements/models.py
+++ mysite2/mysite2/project_managements/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    name = models.CharField('Name', max_length=200)
+    start_date = models.DateField('Start date', null=True, blank=True)
+    end_date = models.DateField('End date', null=True, blank=True)
+    client = models.ForeignKey('Client', on_delete=models.SET_NULL, null=True)
+    project_manager = models.ForeignKey('Project manager', on_delete=models.SET_NULL, null=True)
+    employees = models.ForeignKey('Employees', on_delete=models.SET_NULL, null=True)
+    
+
+
+    def __str__(self):
+        return self.name
