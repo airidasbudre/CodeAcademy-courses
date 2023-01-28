3758231b658d5ccce7e18b94de277d68beaaee10
airidasbudre <budreckis.airidas@gmail.com>
2023-01-28 20:54:08 +0200

    Update

--- mysite2/mysite2/project_managements/admin.py
+++ mysite2/mysite2/project_managements/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Project, Client, Employee, Work, Invoice
 
 # Register your models here.
+admin.site.register(Project)
+admin.site.register(Client)
+admin.site.register(Employee)
+admin.site.register(Work)
+admin.site.register(Invoice)
