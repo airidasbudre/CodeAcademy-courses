6d5582ffa0f3d957c0ca2be57fc58ee8e03188c5
airidasbudre <107766475+airidasbudre@users.noreply.github.com>
2023-02-05 16:49:13 +0300

    Models, views, urls and templates create

--- mysite3/to_do/models.py
+++ mysite3/to_do/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Task(models.Model):
+    text = models.TextField(verbose_name="Text", max_length=1000)
+    user = models.ForeignKey(to=User, verbose_name="User", on_delete=models.SET_NULL, null=True, blank=True)
+    date = models.DateTimeField("Date", auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.text} {self.date}'
+
+
--- mysite3/to_do/views.py
+++ mysite3/to_do/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Task
 
-# Create your views here.
+def index(request):
+    num_task = Task.objects.count()
+   
+    context = {
+        "num_task" : num_task,
+    }
+    return render(request, 'base.html', context=context)
