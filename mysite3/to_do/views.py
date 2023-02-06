7f03de51bbe9bd2150f47c0f7faee9b562b8b3cd
airidasbudre <107766475+airidasbudre@users.noreply.github.com>
2023-02-06 23:05:46 +0300

    login system

--- mysite3/to_do/views.py
+++ mysite3/to_do/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Task
 
 def index(request):
@@ -8,3 +9,31 @@ def index(request):
         "num_task" : num_task,
     }
     return render(request, 'base.html', context=context)
+
+@login_required
+def view_tasks(request):
+    tasks = Task.objects.filter(user=request.user)
+    return render(request, 'tasks.html', {'tasks': tasks})
+
+@login_required
+def create_task(request):
+    if request.method == 'POST':
+        task = Task(text=request.POST['text'], user=request.user)
+        task.save()
+        return redirect('view_tasks')
+    return render(request, 'create_task.html')
+
+@login_required
+def edit_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    if request.method == 'POST':
+        task.text = request.POST['text']
+        task.save()
+        return redirect('view_tasks')
+    return render(request, 'edit_task.html', {'task': task})
+
+@login_required
+def delete_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.delete()
+    return redirect('view_tasks')
